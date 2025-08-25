43a2f76107afe7a3def06e63f71eca8d12cb18b0
zouanni.zan <zouanni.zan@alibaba-inc.com>
2025-08-25 20:19:23 +0800

    change experimental parameters

--- anni_scripts/run_debug_0825.sh
+++ anni_scripts/run_debug_0825.sh
@@ -81,7 +81,7 @@ python3 -m beyondagent.main_ppo \
     hybrid_experience_training.train_sample_keepratio=${train_sample_keepratio} \
     actor_rollout_ref.rollout.sparse=True \
     algorithm.adv_estimator=grpo \
-    data.train_batch_size=4 \
+    data.train_batch_size=32 \
     data.max_prompt_length=4096 \
     data.max_response_length=20480 \
     data.filter_overlong_prompts=True \
@@ -95,7 +95,7 @@ python3 -m beyondagent.main_ppo \
     actor_rollout_ref.rollout.use_qwen3=False \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=2 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=8 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
@@ -109,7 +109,7 @@ python3 -m beyondagent.main_ppo \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.mode=async \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
-    actor_rollout_ref.rollout.n=4 \
+    actor_rollout_ref.rollout.n=8 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.use_kl_in_reward=False \
